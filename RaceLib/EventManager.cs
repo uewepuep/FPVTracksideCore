52a842530ea054cfd73f806c53f01c0b690930b3
Dan Vogel <dan@melbournephotos.net.au>
2023-06-07 09:54:48 +1000

    copy paste clearing

--- RaceLib/EventManager.cs
+++ RaceLib/EventManager.cs
@@ -515,7 +515,7 @@ namespace RaceLib
 
         public void AddPilotsFromLines(IEnumerable<string> pilots)
         {
-            IEnumerable<Tuple<Pilot, Channel>> pcs = GetPilotsFromLines(pilots, true);
+            IEnumerable<Tuple<Pilot, Channel>> pcs = GetPilotsFromLines(pilots);
             foreach (Tuple<Pilot, Channel> pc in pcs)
             {
                 Pilot p = pc.Item1;
