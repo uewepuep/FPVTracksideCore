6f62b6b3014bfb4d181804740b5b1c141106d787
Dan Vogel <dan@melbournephotos.net.au>
2025-08-15 15:56:22 +1000

    stages save and load

--- DB/Round.cs
+++ DB/Round.cs
@@ -26,12 +26,22 @@ namespace DB
 
         public string GameTypeName { get; set; }
 
+        public Guid Stage { get; set; }
+
         public Round() { }
 
         public Round(RaceLib.Round obj)
             : base(obj)
         {
-            
+            if (obj.Stage != null)
+                Stage = obj.Stage.ID;
+        }
+
+        public override RaceLib.Round GetRaceLibObject(ICollectionDatabase database)
+        {
+            RaceLib.Round round = base.GetRaceLibObject(database);
+            round.Stage = Stage.Convert<RaceLib.Stage>(database);
+            return round;
         }
     }
 }
