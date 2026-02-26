e2bec5effbf9595f4824024c6d8fa9892a3fedc5
Dan Vogel <dan@melbournephotos.net.au>
2026-02-27 08:47:34 +1100

    version number inc

--- TimingTesting/Properties/AssemblyInfo.cs
+++ TimingTesting/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.73.0.750")]
-[assembly: AssemblyFileVersion("2.73.0.750")]
+[assembly: AssemblyVersion("2.73.1.751")]
+[assembly: AssemblyFileVersion("2.73.1.751")]
