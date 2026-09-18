8d3556587b5adfe7964ad81126e1916fd8b1da33
Dan Vogel <dan@melbournephotos.net.au>
2026-09-18 10:55:50 +1000

    Changes the way user settings are overwritten/not overwritten

--- FPVMacSideCore/MacPlatformTools.cs
+++ FPVMacSideCore/MacPlatformTools.cs
@@ -103,7 +103,12 @@ namespace FPVMacsideCore
                             newDirectory = WorkingDirectory.CreateSubdirectory(copy);
                         }
 
-                        IOTools.CopyDirectory(oldDirectory, newDirectory, IOTools.Overwrite.IfNewer);
+                        // Never overwrite - anything already in the user's copy (themes,
+                        // scripts, sounds, etc.) may have been hand-edited, and there's no
+                        // reliable way to tell an intentional user edit apart from a newer
+                        // release's file using timestamps alone. Only files missing entirely
+                        // get seeded from this release.
+                        IOTools.CopyDirectory(oldDirectory, newDirectory, IOTools.Overwrite.Never);
                     }
                 }
                 catch (Exception e)
